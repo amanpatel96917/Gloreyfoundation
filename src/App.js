d98745de032db7d82d32137147f139f8879db32a
Aman <amanpatel96917@gmail.com>
2023-08-28 06:17:24 -0700

    Add this page into routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./Pages/Home";
 import Navbar from "./Component/Navbar";
 import About from "./Pages/About";
 import Footer from "./Component/Footer";
+import Services from "./Pages/Services";
+import Pricing from "./Pages/Pricing";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer/>
       </BrowserRouter>
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -7,19 +7,27 @@ export default function Navbar() {
         <nav className="navbar">
             <div className="container">
                 <div className="logo">
-                    <Link to='/'><img src="./images/Logo_glorey.png" alt="Logo" style={{maxWidth:"18%"}} /></Link>
+                    <Link to='/'><img src="./images/Logo_glorey.png" alt="Logo" style={{ maxWidth: "18%" }} /></Link>
                 </div>
                 <ul className="nav-links">
                     <input type="checkbox" id="checkbox_toggle" />
                     <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
                     <div className="menu">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li className="services">
-                            <Link to="/">Services</Link>
+                        <li>
+                            <Link to="/">Home</Link>
                         </li>
-                        <li><Link to="/">Pricing</Link></li>
-                        <li><Link to="/">Contact</Link></li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/services">Services</Link>
+                        </li>
+                        <li>
+                            <Link to="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
                     </div>
                 </ul>
             </div>
